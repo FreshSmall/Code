eb287125671548e8b067600acc170cd41dc10e80
FreshSmall <26374124+FreshSmall@users.noreply.github.com>
2017-12-22 23:27:44 +0800

    添加一个新的测试类

---
+++ DataPoolDemo/src/cn/com/pool/DataPoolUtil.java
@@ -0,0 +1,10 @@
+package cn.com.pool;
+
+/**
+ * Created by yinchao on 2017/12/22.
+ */
+public class DataPoolUtil {
+    public static void main(String[] args){
+        System.out.println("测试数据库连接池！");
+    }
+}
